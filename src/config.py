041bd93f6ff5e0707c118e29e03bc5954876ecca
grape0919 <30318115+grape0919@users.noreply.github.com>
2023-07-18 15:53:29 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ import os,sys
 
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-ADMIN_KEY = "bb7381f8eb0cf783384acf578e563192"
-CLIENT_ID = "05d1753e4d9f2c1ec80b04288ed8085f"
+ADMIN_KEY = ""
+CLIENT_ID = ""
 REDIRECT_URI = "http://localhost:7055/auth"
